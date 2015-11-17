f0ce79e62521513a9227b4b66499d44b96cd1d24
adorechic <adorechic@gmail.com>
2015-11-17 20:45:31 +0900

    Initial commit

---
+++ lib/fluent/plugin/validate/tag/filter.rb
@@ -0,0 +1,13 @@
+require "fluent/plugin/validate/tag/filter/version"
+
+module Fluent
+  module Plugin
+    module Validate
+      module Tag
+        module Filter
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
---
+++ lib/fluent/plugin/validate/tag/filter/version.rb
@@ -0,0 +1,11 @@
+module Fluent
+  module Plugin
+    module Validate
+      module Tag
+        module Filter
+          VERSION = "0.1.0"
+        end
+      end
+    end
+  end
+end
